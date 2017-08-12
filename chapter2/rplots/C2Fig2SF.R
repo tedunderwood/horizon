28a3a7308ae76af65c2a1b37c5c9f7a529c1feeb
tedunderwood <tunder@illinois.edu>
2017-08-12 09:23:30 -0500

    changing font to Avenir Next Medium

---
+++ chapter2/rplots/C2Fig2SF.R
@@ -0,0 +1,25 @@
+library(scales)
+library(ggplot2)
+library(dplyr)
+l <- read.csv('../modeloutput/allSF.csv')
+l$realclass <- as.character(l$realclass)
+l$realclass[l$realclass == '1'] <- 'SF'
+l$realclass[l$realclass == '0'] <- 'random'
+l$reviewed <- as.factor(l$realclass)
+
+textframe = data.frame(label = c("L'An 2440"), x = c(1800), y = c(.7))
+
+p <- ggplot(l, aes(x = dateused, y = logistic, color = reviewed, shape = reviewed)) + 
+  geom_point() + scale_shape_manual(name="actually\n", values = c(3, 17)) + 
+  scale_color_manual(name = "actually\n", values = c('gray40', 'black')) + 
+  theme_bw() + theme(text = element_text(size = 16, family = 'Avenir Next Medium')) + 
+  scale_y_continuous('', labels = percent, breaks = c(0.25,0.5,0.75)) + 
+  scale_x_continuous("", breaks = c(1800,1850,1900,1950,2000)) +
+  ggtitle('Predicted probability of being science fiction') +
+  theme(plot.title = element_text(hjust = 0, margin = margin(b = 10), size = 16))
+plot(p)
+
+tiff("../images/C2Fig2allSF.tiff", height = 6, width = 9, units = 'in', res=400)
+plot(p)
+dev.off()
+plot(p)
--- chapter2/rplots/C2Fig3paceofchange.R
+++ chapter2/rplots/C2Fig3paceofchange.R
@@ -14,16 +14,14 @@ allpace <- rbind(sfpace, detectpace)
 allpace$iteration <- as.factor(allpace$iteration)
 
 p <- ggplot(allpace, aes(x = date, y = difference, shape = iteration, color = genre, linetype = genre)) + 
-  scale_color_manual(values = c('gray50', 'black')) +
   geom_line(size = 1) +
   theme_bw() + scale_y_continuous('', labels = percent) +
   scale_x_continuous('', breaks = c(1890, 1910, 1930, 1950, 1970)) +
   ggtitle('Accuracy lost in sliding a model forward or back 30 years') +
-  theme(plot.title = element_text(hjust = -0.12, size = 18, vjust = 1.5)) +
-  theme(text = element_text(size = 18, family = "Baskerville"), panel.border = element_blank()) +
+  theme(text = element_text(size = 18, family = "Avenir Next Medium"), panel.border = element_blank()) +
   theme(axis.line = element_line(color = 'black'),
         axis.text = element_text(color = 'black'),
-        axis.title.x = element_text(margin = margin(t = 14), size = 16)) +
+        plot.title = element_text(margin = margin(b = 12), hjust = 0, size = 14)) +
   scale_color_manual(name = 'genre\n', values = c('gray60', 'black'),
                      guide = guide_legend(keyheight = 3,  label.vjust = 0.55, 
                                           override.aes = list(fill = NA, size = 1))) +

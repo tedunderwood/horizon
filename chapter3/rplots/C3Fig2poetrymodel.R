04d34d391b24a47fb05733a05b39290110ec7299
tedunderwood <tunder@illinois.edu>
2017-08-12 15:30:24 -0500

    adding R scripts

---
+++ chapter3/rplots/C3Fig2poetrymodel.R
@@ -0,0 +1,30 @@
+library(scales)
+library(ggplot2)
+l <- read.csv('~/Dropbox/book/chapter3/results/fullpoetry.results.csv')
+
+model = lm(data = l, formula = logistic ~ dateused)
+intercept = coef(model)[1]
+slope = coef(model)[2]
+
+line = intercept + (slope * l$pubdate)
+l$reviewed = as.factor(l$realclass)
+levels(l$reviewed) = c('random', 'reviewed')
+p <- ggplot(l, aes(x = pubdate, y = logistic, color = reviewed, shape = reviewed)) + theme_bw() +
+  geom_point() + geom_abline(intercept = intercept, slope = slope) + scale_shape_manual(name="actually\n", values = c(1, 17)) + 
+  scale_color_manual(name = "actually\n", values = c('gray40', 'gray0')) + 
+  theme(text = element_text(size = 16, family = 'Avenir Next Medium')) + 
+  scale_y_continuous('', labels = percent, breaks = c(0.25,0.5,0.75)) + 
+  scale_x_continuous("", breaks = c(1820, 1840, 1860, 1880, 1900, 1920)) +
+  ggtitle('Predicted probability of belonging to reviewed set') +
+  theme(plot.title = element_text(hjust = 0, margin = margin(b = 10), size = 16))
+
+tiff("~/Dropbox/book/chapter3/images/poetrymodel.tiff", height = 6, width = 9, units = 'in', res=400)
+plot(p)
+dev.off()
+plot(p)
+
+tp = sum(l$realclass == 1 & l$logistic >= 0.5)
+tn = sum(l$realclass == 0 & l$logistic < 0.5)
+fp = sum(l$realclass == 0 & l$logistic >= 0.5)
+fn = sum(l$realclass == 1 & l$logistic < 0.5)
+print((tp + tn) / (tp+tn+fp+fn))

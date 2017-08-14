a0a9eb95dfaa286657dd83c6470bd7c51cf3aeb3
tedunderwood <tunder@illinois.edu>
2017-08-14 15:53:24 -0500

    figs 4.2-4.6

---
+++ chapter4/rplots/C4Fig1accuracybydecade.R
@@ -0,0 +1,27 @@
+# Predictive accuracy by decade
+
+library(scales)
+library(ggplot2)
+library(dplyr)
+
+this.dir <- dirname(parent.frame(2)$ofile)
+setwd(this.dir)
+
+d2 <- read.csv('../dataforR/decade_optimums.tsv', sep = '\t')
+library(ggplot2)
+library(scales)
+
+p <- ggplot(d2, aes(x = decade, y = accuracy)) + 
+  geom_point(color = 'black', alpha = 0.6, shape = 23, fill = 'gray70', size = 2.5) +
+  scale_y_continuous('', labels = percent) +
+  xlab('') + ggtitle('Accuracy of gender prediction') +
+  theme_bw() + 
+  theme(text = element_text(size = 18, family = "Avenir Next Medium"), panel.border = element_blank()) +
+  theme(axis.line = element_line(color = 'black'),
+        axis.text = element_text(color = 'black'),
+        plot.title = element_text(margin = margin(b = 14), size = 16))
+
+tiff("../images/C4Fig1accuracy.tiff", height = 6, width = 9, units = 'in', res=400)
+plot(p)
+dev.off()
+plot(p)
---
+++ chapter4/rplots/C4Fig6eyeshairchestpocket.R
@@ -0,0 +1,34 @@
+# Plotting words
+
+library(scales)
+library(ggplot2)
+library(reshape2)
+
+this.dir <- dirname(parent.frame(2)$ofile)
+setwd(this.dir)
+
+df <- read.csv('../dataforR/diff_matrix.csv')
+df <- df[df$thedate > 1799, ]
+
+subset <- select(df, thedate, eyes, hair, chest, pocket)
+
+longform <- melt(subset, id.vars = c('thedate'))
+
+p <- ggplot(longform, aes(x = thedate, y = value, color = variable, shape = variable, linetype = variable)) + 
+  geom_point() + geom_smooth(span = 0.4, se = FALSE, show.legend = FALSE) +
+  scale_linetype_manual(guide = 'none', values = c('solid', 'solid', 'dashed', 'dashed')) +
+  scale_color_manual(name = 'word\n', values = c('black', 'gray60', 'black', 'gray40')) +
+  scale_shape_manual(name = 'word\n', values = c(16, 15, 2, 3)) +
+  theme(text = element_text(size = 17)) + 
+  scale_y_continuous('') + ggtitle('Difference in frequency, women - men') +
+  scale_x_continuous("", breaks = c(1800,1850,1900,1950,2000)) +
+  theme_bw() + 
+  theme(text = element_text(size = 18, family = "Avenir Next Medium"), panel.border = element_blank()) +
+  theme(axis.line = element_line(color = 'black'),
+        axis.text = element_text(color = 'black'),
+        plot.title = element_text(margin = margin(b = 14), size = 17))
+
+tiff("../images/C4Fig6eyeshairchest.tiff", height = 6, width = 9, units = 'in', res=400)
+plot(p)
+dev.off()
+plot(p)

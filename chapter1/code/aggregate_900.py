f931b864d2a8b73f3602e7e59618a9c58a6c0cac
tedunderwood <tunder@illinois.edu>
2017-12-07 14:16:44 -0600

    documenting aggregation step

---
+++ chapter1/code/aggregate_900.py
@@ -0,0 +1,16 @@
+## aggregate the nine hundred
+
+import csv
+import pandas as pd
+
+root = '../modeloutput/'
+frames = []
+for floor in range(1700, 2000, 50):
+    sourcefile = root + 'theninehundred' + str(floor) + '.csv'
+    thisframe = pd.read_csv(sourcefile)
+    frames.append(thisframe)
+
+df = pd.concat(frames)
+df.head()
+
+df.to_csv('../plotdata/the900.csv', index = False)

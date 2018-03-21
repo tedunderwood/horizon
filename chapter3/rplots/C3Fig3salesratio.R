6b174bf0baf47bc21bb679175ad6642851664ac8
tedunderwood <tunder@illinois.edu>
2018-03-21 13:30:04 -0500

    simplifying figure 3

--- chapter3/rplots/C3Fig3salesratio.R
+++ chapter3/rplots/C3Fig3salesratio.R
@@ -4,16 +4,19 @@ this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 data <- read.csv('../salesdata/dataforC3Fig3.csv')
 
+data$ratiomax[data$ratiomax > 10] <- 10
+
 p <- ggplot(data, aes(x = year)) +
   geom_line(aes(y = ratio), color = 'black') +
   geom_ribbon(aes(ymin = ratiomin, ymax = ratiomax), fill = 'gray60', alpha = 0.4) +
   theme_bw() +
-  ggtitle("Ratio of reviewed authors' sales to random authors' sales") +
+  ggtitle("Ratio of bestsellers by reviewed authors\nto those by random authors") +
   labs(x = '', y = '') +
+  scale_y_continuous(limits = c(0, 10), breaks = c(0, 1, 2.5, 5, 10)) +
   theme(text = element_text(size = 18, family = "Avenir Next Medium"), panel.border = element_blank()) +
   theme(axis.line = element_line(color = 'black'), 
         legend.position = 'none',
-        plot.title = element_text(size = 18, margin = margin(b = 16)),
+        plot.title = element_text(size = 18, margin = margin(b = 16), lineheigh = 1.1),
         axis.title.x = element_text(margin = margin(t = 14)),
         axis.title.y = element_text(margin = margin(r = 14)))
 

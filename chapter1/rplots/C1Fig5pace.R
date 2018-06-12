adcaaf4e1df7363cdfbf745bd76e7e265e23250a
tedunderwood <tunder@illinois.edu>
2018-06-12 15:41:56 -0500

    documenting plot scripts

--- chapter1/rplots/C1Fig5pace.R
+++ chapter1/rplots/C1Fig5pace.R
@@ -1,32 +1,24 @@
 # Make unweighted meantime graph
-library(scales)
 library(ggplot2)
 library(dplyr)
-
-this.dir <- dirname(parent.frame(2)$ofile)
-setwd(this.dir)
-
 times <- read.csv('../plotdata/averagetimes.csv')
-# fic <- filter(times, col != 'red')
-timebreaks = c(-4.82, -2.81, -1.43, 0.365, 1.75, 3.7, 5.08, 7.650835, 10.65)
-timelabels = c('2 min', '15 min', 'an hour', '6 hours', 'a day', 'a week', 'a month', 'a year', '20 years')
+
+timebreaks = c(-4.82, -2.81, -1.43, 0.365, 1.75, 3.7, 5.08, 7.650835)
+timelabels = c('2 min', '15 min', 'an hour', '6 hours', 'a day', 'a week', 'a month', 'a year')
 times$col <- as.character(times$col)
-times$col[times$col != 'red'] <- '\nfiction'
-times$col[times$col == 'red'] <- '\nbiography and\nautobiography'
+times$col[times$col != 'red'] <- '\nfiction\n'
+times$col[times$col == 'red'] <- '\nbiography and\nautobiography\n'
 
-p <- ggplot(times, aes(x = meanhard, y = meantime, shape = col)) + geom_point() + 
-  theme_bw() + 
+p <- ggplot(times, aes(x = date, y = meantime, shape = col, linetype = col)) + geom_point() + 
+  geom_smooth(color = 'black', show.guides = FALSE) +
+  theme_bw() + scale_linetype_manual(guide = 'none', values = c('dashed', 'solid')) +
   scale_y_continuous('', breaks = timebreaks, labels = timelabels) +
-  scale_x_continuous('ratio of words in Stanford "concrete" list to all words') +
-  scale_shape_manual(name = 'genre', values = c(2, 16), 
-                     guide = guide_legend(keyheight = 3,  label.vjust = 1)) +
-  annotate('text', x = 0, y = 9.5, label = 'Time narrated in 250 words', 
-           hjust = 0, family = 'Baskerville', size = 6) +
-  theme(text = element_text(size = 18, family = "Baskerville"), panel.border = element_blank()) +
-  theme(axis.line = element_line(color = 'black'),
-        axis.text = element_text(color = 'black'),
-        axis.title.x = element_text(margin = margin(t = 14), size = 16))
-
+  scale_x_continuous('', breaks = c(1719, 1800, 1900, 2000)) +
+  scale_shape_manual(name = 'genre\n', values = c(1, 17)) +
+  theme(text = element_text(size = 20, family = "Avenir Next Medium"), panel.border = element_blank()) +
+  theme(axis.line = element_line(color = 'black')) +
+  annotate('text', x = 1715, y = 9.55, label = 'mean time narrated\nin 250 words', 
+           hjust = 0, family = "Avenir Next Medium", size = 5)
 tiff("../images/C1Fig5pace.tiff", height = 5.5, width = 9, units = 'in', res=400)
 plot(p)
 dev.off()
---
+++ chapter1/rplots/readme.md
@@ -0,0 +1,6 @@
+visualization scripts
+======================
+
+Mostly self-explanatory, but note that the "boxplot" figure no longer actually uses boxplots. (Boxplots are super-informative for an audience that is used to them, but I don't expect the audience for this book to be used to them, so I turned it into a jitterplot.)
+
+These names should align to the names of images in the [**../images**](https://github.com/tedunderwood/horizon/tree/master/chapter1/images) folder.

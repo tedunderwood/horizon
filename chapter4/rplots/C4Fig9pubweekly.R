2f464dfec2e38f87f73b8772e9f5ef3bcb225a22
tedunderwood <tunder@illinois.edu>
2018-06-14 10:21:19 -0500

    updating viz scripts

--- chapter4/rplots/C4Fig9pubweekly.R
+++ chapter4/rplots/C4Fig9pubweekly.R
@@ -6,7 +6,7 @@ this.dir <- dirname(parent.frame(2)$ofile)
 setwd(this.dir)
 
 errors <- read.csv('../dataforR/pubweeklyerrorbars.csv')
-hathi <- read.csv('../dataforR/authorratios.csv')
+hathi <- read.csv('../dataforR/authorratios_nojuv.csv')
 
 p <- ggplot() + 
   geom_errorbar(data=errors, mapping=aes(x=year, ymin=low, ymax=high), width=5, size=1, color="black") + 
@@ -14,16 +14,14 @@ p <- ggplot() +
   geom_point(data = hathi, mapping = aes(x = year, y = authratio), shape = 18, size = 2, alpha = 0.5) + 
   ylab('') +
   scale_y_continuous(labels = percent, limits = c(0, 0.8)) +
-  xlab('') + theme(text = element_text(size = 15)) +
-  ggtitle('Percentage of fiction by women in HathiTrust (points)\nand Publishers Weekly (error bars)') +
+  xlab('') +
   xlim(1800, 2007) + 
   theme_bw() +
-  theme(text = element_text(size = 16, family = "Avenir Next Medium"), 
+  theme(text = element_text(size = 18, family = "Avenir Next Medium"), 
         panel.border = element_blank(),
-        axis.line = element_line(color = 'black'),
-        plot.title = element_text(margin = margin(b = 14), size = 16, lineheight = 1.1))
+        axis.line = element_line(color = 'black'))
 
-tiff("../images/C4Fig9alternate.tiff", height = 6, width = 9, units = 'in', res=400)
+tiff("../images/C4Fig9pubweekly_nojuv.tiff", height = 6, width = 9, units = 'in', res=400)
 plot(p)
 dev.off()
 plot(p)
